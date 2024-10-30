b34f32fac0bf0c920e48edead8c41298d56a7a29
BenjaminCleon <benjamin.cleon@outlook.fr>
2024-10-30 14:39:52 +0100

    adpatation

--- Exo5/domain_kvm.c
+++ Exo5/domain_kvm.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <libvirt/libvirt.h>
 
 void print_hostname(virConnectPtr conn);
-void list_domain_infos(virConnectPtr conn);
+void list_domain_infos(virConnectPtr conn, char *state);
 
 int main(int argc, char *argv[])
 {
@@ -42,15 +43,15 @@ int main(int argc, char *argv[])
 
 void list_domain_infos(virConnectPtr conn, char *state)
 {
-    virDomainInfoPtr *domains_info;
+    virDomainInfoPtr domains_info;
 
     virDomainPtr *domains;
     int num_domains;
-    if ( state == "active")
+    if (   strcmp(state,"active") == 0 )
     {
         num_domains = virConnectListAllDomains(conn, &domains, VIR_CONNECT_LIST_DOMAINS_ACTIVE);
     }
-    else if (state == "inactive")
+    else if ( strcmp(state,"inactive") == 0 )
     {
         num_domains = virConnectListAllDomains(conn, &domains, VIR_CONNECT_LIST_DOMAINS_INACTIVE);
     }
@@ -60,11 +61,12 @@ void list_domain_infos(virConnectPtr conn, char *state)
         virDomainGetInfo(domains[i], domains_info);
 
         printf("id: %d, nom: %s\n", virDomainGetID(domains[i]), virDomainGetName(domains[i]));
-        printf("state: %c\n", domains_info->state);
-        printf("maxMem: %ld\n", domains_info->maxMem);
-        printf("memory: %ld\n", domains_info->memory);
+        printf("state: %c\n"    , domains_info->state    );
+        printf("maxMem: %ld\n"  , domains_info->maxMem   );
+        printf("memory: %ld\n"  , domains_info->memory   );
         printf("nrVirtCpu: %d\n", domains_info->nrVirtCpu);
-        printf("cpuTime: %ld\n", domains_info->cpuTime);   
+        printf("cpuTime: %ld\n" , domains_info->cpuTime  );   
+        
         virDomainFree(domains[i]);
     }
 
