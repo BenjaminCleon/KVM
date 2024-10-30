46919100f0528c4c7fda45f6f656afc3812de8d4
BenjaminCleon <benjamin.cleon@outlook.fr>
2024-10-30 15:04:49 +0100

    return

--- Exo5/domain_kvm.c
+++ Exo5/domain_kvm.c
@@ -22,20 +22,20 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-    printf("Connection opened successfully\n");
-    
-    // Hostname
-    print_hostname(conn);
+	printf("Connection opened successfully\n");
+	
+	// Hostname
+	print_hostname(conn);
 
-    // List
+	// List
 
-    // List Active VM
-    printf("Active domains:\n");
-    list_domain_infos(conn, "active");
+	// List Active VM
+	printf("Active domains:\n");
+	list_domain_infos(conn, "active");
 
-    // List Inactive VM
-    printf("Inactive domains:\n");
-    list_domain_infos(conn, "inactive");
+	// List Inactive VM
+	printf("Inactive domains:\n");
+	list_domain_infos(conn, "inactive");
 
 	virConnectClose(conn);
 	return 0;
@@ -43,48 +43,46 @@ int main(int argc, char *argv[])
 
 void list_domain_infos(virConnectPtr conn, char *state)
 {
-    virDomainInfoPtr domains_info;
+	virDomainInfoPtr domains_info;
 
-    virDomainPtr *domains;
-    int num_domains;
-    if (   strcmp(state,"active") == 0 )
-    {
-        num_domains = virConnectListAllDomains(conn, &domains, VIR_CONNECT_LIST_DOMAINS_ACTIVE);
-    }
-    else if ( strcmp(state,"inactive") == 0 )
-    {
-        num_domains = virConnectListAllDomains(conn, &domains, VIR_CONNECT_LIST_DOMAINS_INACTIVE);
-    }
-    
-    puts("Test\n");
+	virDomainPtr *domains;
+	int num_domains;
+	if (   strcmp(state,"active") == 0 )
+	{
+		num_domains = virConnectListAllDomains(conn, &domains, VIR_CONNECT_LIST_DOMAINS_ACTIVE);
+	}
+	else if ( strcmp(state,"inactive") == 0 )
+	{
+		num_domains = virConnectListAllDomains(conn, &domains, VIR_CONNECT_LIST_DOMAINS_INACTIVE);
+	}
 
-    for (int i = 0; i < num_domains; i++) 
-    {
-        virDomainGetInfo(domains[i], domains_info);
+	for (int i = 0; i < num_domains; i++) 
+	{
+		virDomainGetInfo(domains[i], domains_info);
 
-        printf("id: %d, nom: %s\n", virDomainGetID(domains[i]), virDomainGetName(domains[i]));
-        printf("state: %c\n"    , domains_info->state    );
-        printf("maxMem: %ld\n"  , domains_info->maxMem   );
-        printf("memory: %ld\n"  , domains_info->memory   );
-        printf("nrVirtCpu: %d\n", domains_info->nrVirtCpu);
-        printf("cpuTime: %lld\n", domains_info->cpuTime  );   
-        
-        virDomainFree(domains[i]);
-    }
+		printf("id: %d, nom: %s\n", virDomainGetID(domains[i]), virDomainGetName(domains[i]));
+		printf("state: %c\n"    , domains_info->state    );
+		printf("maxMem: %ld\n"  , domains_info->maxMem   );
+		printf("memory: %ld\n"  , domains_info->memory   );
+		printf("nrVirtCpu: %d\n", domains_info->nrVirtCpu);
+		printf("cpuTime: %lld\n", domains_info->cpuTime  );   
+		
+		virDomainFree(domains[i]);
+	}
 
-    puts("Test1\n");
-
-    if (num_domains == 0) printf("No %s domains\n", state);
-    else                  virDomainFree(domains);
-    
-    puts("Test2\n");
+	if (num_domains == 0) printf("No %s domains\n", state);
+	else                  free(domains);
+	
+	return;
 }
 
 void print_hostname(virConnectPtr conn)
 {
-    char *host;
+	char *host;
 
-    host = virConnectGetHostname(conn);
+	host = virConnectGetHostname(conn);
 	fprintf(stdout, "Hostname:%s\n", host);
 	free(host);
+
+	return;
 }
